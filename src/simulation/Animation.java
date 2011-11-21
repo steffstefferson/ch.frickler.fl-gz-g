6b159fddf0d647f72aa8fb02ab97f281e128d97c
krigu <krigu@hadhfang.(none)>
2011-11-21 16:49:48 +0100

    Blubiblabla

---
+++ src/simulation/Animation.java
@@ -0,0 +1,12 @@
+
+
+public class Animation extends JFrame{
+
+
+// Liste mit allen Flugzeigen die geziechnet werden sollen
+HashTable<Aircraft, Object> querySet = new Hashtable();
+
+constructor 
+	// Zeichnen 
+
+}
