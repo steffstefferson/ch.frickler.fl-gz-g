f012b58362876fce935677d0471444f44b609e33
Lukas Kuster <koschder@gmail.com>
2012-01-15 10:02:49 +0100

    seems to be working...

--- src/simulation/communication/MPICommunication.java
+++ src/simulation/communication/MPICommunication.java
@@ -8,6 +8,8 @@ import simulation.model.SimWorld;
 
 public class MPICommunication implements Communication {
 
+	Request[] requests = new Request[4];
+
 	@Override
 	public void send(Event event, Aircraft aircraft) {
 		System.out.println("sending Event " + event.toString());
@@ -18,8 +20,8 @@ public class MPICommunication implements Communication {
 		// 0, 1, MPI.OBJECT,
 		// dest, 1);
 
-		if (MPI.COMM_WORLD.Rank() == 0)
-			MPI.COMM_WORLD.Ssend(new int[] { event.getType() }, 0, 1, MPI.INT, dest, 1);
+		// if (MPI.COMM_WORLD.Rank() == 0)
+		MPI.COMM_WORLD.Send(new int[] { event.getType() }, 0, 1, MPI.INT, dest, 1);
 	}
 
 	@Override
@@ -28,15 +30,23 @@ public class MPICommunication implements Communication {
 		int[] messages = new int[1];
 		int n = MPI.COMM_WORLD.SizeTotal();
 		Request request = null;
-		// for (int i = 0; i < n; i++) {
-		// request = MPI.COMM_WORLD.Irecv(messages, 0, 1, MPI.OBJECT, i, 1);
+		for (int i = 0; i < n; i++) {
+			if (i == MPI.COMM_WORLD.Rank())
+				continue;
+			if (requests[i] == null)
+				requests[i] = MPI.COMM_WORLD.Irecv(messages, 0, 1, MPI.INT, i, 1);
 
-		if (MPI.COMM_WORLD.Rank() != 0) {
-			request = MPI.COMM_WORLD.Irecv(messages, 0, 1, MPI.INT, 0, 1);
+			// if (MPI.COMM_WORLD.Rank() != 0) {
+			// request = MPI.COMM_WORLD.Irecv(messages, 0, 1, MPI.INT, 0, 1);
 			// System.out.println("Received Request " + request.toString() +
 			// "from " + 0);
-			// if (request.Test() != null)
-			request.Wait();
+			if (requests[i].Test() != null) {
+				System.out.println("Receiving...");
+				requests[i].Wait();
+//				MPI.COMM_WORLD.Recv(messages, 0, 1, MPI.INT, i, 1);
+				requests[i] = null;
+			}
+			// request.Wait();
 			if (messages[0] != 0)
 				System.out.println("Message is " + messages[0]);
 			// System.out.println("Received Event "+ messages[0] + " from " +i);
