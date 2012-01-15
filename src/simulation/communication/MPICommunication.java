02b8fec2876fa893f0fd309581b2874f51f0ffd9
Lukas Kuster <koschder@gmail.com>
2012-01-15 10:58:22 +0100

    further simplification

--- src/simulation/communication/MPICommunication.java
+++ src/simulation/communication/MPICommunication.java
@@ -12,8 +12,8 @@ public class MPICommunication implements Communication {
 
 	@Override
 	public void send(Event event, Aircraft aircraft) {
-		Message[]  m= new Message[] { new Message(event, aircraft) };
-		System.out.println("sending Message " +m[0]);
+		Message[] m = new Message[] { new Message(event, aircraft) };
+		System.out.println("sending Message " + m[0]);
 		int n = MPI.COMM_WORLD.SizeTotal();
 		final int dest = aircraft.getDestination().getAirportId() % n;
 		System.out.println("n = " + n + ",dest = " + dest);
@@ -22,19 +22,16 @@ public class MPICommunication implements Communication {
 
 	@Override
 	public Message receive() {
-			if (request == null)
-				request = MPI.COMM_WORLD.Irecv(messages, 0, 1, MPI.OBJECT, MPI.ANY_SOURCE, 1);
+		if (request == null)
+			request = MPI.COMM_WORLD.Irecv(messages, 0, 1, MPI.OBJECT, MPI.ANY_SOURCE, 1);
 
-			if (request.Test() != null) {
-				System.out.println("Receiving...");
-				request = null;
-			}
-			if (messages[0] != null) {
-				System.out.println("Received message is " + messages[0]);
-				Message retMessage = messages[0];
-				messages[0] = null;
-				return retMessage;
-			}
+		if (request.Test() != null) {
+			System.out.println("Received message is " + messages[0]);
+			Message retMessage = messages[0];
+			request = null;
+			messages[0] = null;
+			return retMessage;
+		}
 		return null;
 	}
 
