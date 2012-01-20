c91b86e138dafccd0472d575d2122620ec51121b
John Ashton <chris.feuz@gmail.com>
2012-01-20 10:52:03 +0100

    More comments

--- src/simulation/EventQueueManager.java
+++ src/simulation/EventQueueManager.java
@@ -7,18 +7,33 @@ import java.util.Set;
 
 import simulation.model.Event;
 
+/**
+ * This class handles all queues. There are three queues: One for all upcoming
+ * events, one for all processed events and one for all anti-messages
+ * 
+ */
 public class EventQueueManager {
 
 	private List<Event> eventList; // time ordered list
 	private Set<Event> antiMessages;
 	private List<Event> processedEvents;
 
+	/**
+	 * Default constructor
+	 */
 	public EventQueueManager() {
 		eventList = new ArrayList<Event>();
 		antiMessages = new HashSet<Event>();
 		processedEvents = new ArrayList<Event>();
 	}
 
+	/**
+	 * Inserts an event to the event-queue. If the message is an anti-message
+	 * the event removes the according event from the queue.
+	 * 
+	 * @param e
+	 *            Event to insert into queue
+	 */
 	public void insertEvent(Event e) {
 		// antimessage and normal message cancel each other out
 		if (e.isAntiMessage()) {
@@ -35,6 +50,12 @@ public class EventQueueManager {
 		}
 	}
 
+	/**
+	 * Inserts an event at the correct place in the event-queue.
+	 * 
+	 * @param e
+	 *            Event to insert into queue
+	 */
 	private void insertAtCorrectTime(Event e) {
 		int pos = 0;
 		while (pos < eventList.size()) {
@@ -47,10 +68,19 @@ public class EventQueueManager {
 		eventList.add(pos, e);
 	}
 
+	/**
+	 * 
+	 * @return List of all processed events
+	 */
 	public List<Event> getProcessedEvents() {
 		return processedEvents;
 	}
 
+	/**
+	 * After a rollback an event can be re-inserted into the queue
+	 * 
+	 * @param e
+	 */
 	public void reInsertEvent(final Event e) {
 		if (e.isAntiMessage())
 			throw new IllegalArgumentException("Cannot re-insert antiMessage, as it should never have been processed");
@@ -58,16 +88,32 @@ public class EventQueueManager {
 		insertEvent(e);
 	}
 
+	/**
+	 * Insert an event to the processed queue. REPAINT_ANIMATION-Events are not
+	 * added the the processed events, because they are not reprocessed later.
+	 * 
+	 * @param e
+	 *            Processed event
+	 */
 	public void moveToProcessedQueue(final Event e) {
+		// repaint event will automatically regenerated
 		if (e.getType() != Event.REPAINT_ANIMATION)
 			processedEvents.add(e);
 		eventList.remove(e);
 	}
 
+	/**
+	 * 
+	 * @return The next event from the queue
+	 */
 	public Event getNextEvent() {
 		return eventList.get(0);
 	}
 
+	/**
+	 * 
+	 * @return Amount of unprocessed events
+	 */
 	public int getNumberOfPendingEvents() {
 		return eventList.size();
 	}
